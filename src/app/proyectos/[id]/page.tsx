e18f155d72ccb412d536117bf0591464aac103fa
nabratte <61842410+nabratte@users.noreply.github.com>
2025-02-04 01:14:09 -0300

    Solving Promise Problems

--- src/app/proyectos/[id]/page.tsx
+++ src/app/proyectos/[id]/page.tsx
@@ -1,20 +1,22 @@
+'use client';
+
 import { notFound } from "next/navigation";
+import proyectos from "../projectlist.json";
 import Link from "next/link";
-import proyectos from "../projectlist.json"
-import type { PageProps } from "./types";
 
-
-export default function Proyecto({ params }: PageProps) {
-  const proyecto = proyectos.find((p) => p.id === params.id);
+export default async function Proyecto({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params; // Esperamos la resolución de la promesa
+  const proyecto = proyectos.find((p) => p.id === id);
 
   if (!proyecto) return notFound();
 
   return (
     <section>
       <h1 className="text-4xl font-bold">{proyecto.titulo}</h1>
-      <img src={proyecto.imagen} alt={proyecto.titulo} className="w-full h-60 object-cover rounded-lg my-4" />
+      <Link href="../../../sub-apps/ToDoApp/ToDoApp.tsx">
+        <img src={proyecto.imagen} alt={proyecto.titulo} className="w-full h-60 object-cover rounded-lg my-4" />
+      </Link>
       <p className="text-gray-400">{proyecto.descripcion}</p>
-      <Link href="/proyectos" className="block mb-4 text-blue-500">← Volver a la lista de proyectos</Link>
     </section>
   );
 }
